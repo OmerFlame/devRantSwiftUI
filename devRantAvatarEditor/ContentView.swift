13d4b2a00477a679bf90af273e6ffe2f4e8f7b51
Omer Shamai <renginald.bagel@gmail.com>
2020-09-13 18:07:57 +0300

    Initial Commit

---
+++ devRantAvatarEditor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  devRantAvatarEditor
+//
+//  Created by Omer Shamai on 9/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ devRantAvatarEditor/devRantAvatarEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  devRantAvatarEditorApp.swift
+//  devRantAvatarEditor
+//
+//  Created by Omer Shamai on 9/13/20.
+//
+
+import SwiftUI
+
+@main
+struct devRantAvatarEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
