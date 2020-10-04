d68623faf64dbaa185da450795908c57eb9c76d7
Omer Shamai <renginald.bagel@gmail.com>
2020-10-04 13:30:12 +0300

    Possibly fixed crash when rant feed JSON results contains a nil settings array (still weird though)

--- devRantAvatarEditor/Data Structures/RantFeed.swift
+++ devRantAvatarEditor/Data Structures/RantFeed.swift
@@ -35,7 +35,7 @@ struct RantFeed: Codable {
     
     let success: Bool
     let rants: [RantInFeed]
-    let settings: RantFeedSettings
+    let settings: RantFeedSettings?
     
     let set: String
     let wrw: Int
@@ -44,4 +44,29 @@ struct RantFeed: Codable {
     let num_notifs: Int
     let unread: RantFeedUnread
     let news: RantFeedNews?
+    
+    private enum CodingKeys: String, CodingKey {
+        case success,
+             rants,
+             settings,
+             set,
+             wrw,
+             dpp,
+             num_notifs,
+             unread,
+             news
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        success = try values.decode(Bool.self, forKey: .success)
+        rants = try values.decode([RantInFeed].self, forKey: .rants)
+        settings = try? values.decode(RantFeedSettings.self, forKey: .settings)
+        set = try values.decode(String.self, forKey: .set)
+        wrw = try values.decode(Int.self, forKey: .wrw)
+        dpp = try? values.decode(Int.self, forKey: .dpp)
+        num_notifs = try values.decode(Int.self, forKey: .num_notifs)
+        unread = try values.decode(RantFeedUnread.self, forKey: .unread)
+        news = try? values.decode(RantFeedNews.self, forKey: .news)
+    }
 }
